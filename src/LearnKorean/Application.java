87312a4509bb4023c736c0b591114793b9d20f85
InspecteurGadget <maltaesousa@gmail.com>
2016-01-26 20:02:59 +0100

    1st commit

---
+++ src/LearnKorean/Application.java
@@ -0,0 +1,10 @@
+package LearnKorean;
+
+public class Application {
+
+  public static void main(String[] args) {
+    MainWindow win0 = new MainWindow("Apprenez le Koréen");
+
+  }
+
+}
---
+++ src/LearnKorean/MainWindow.java
@@ -0,0 +1,23 @@
+package LearnKorean;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame implements ActionListener {
+
+public MainWindow(String title) {
+    super(title);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+    setVisible(true);
+
+  }
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+}
