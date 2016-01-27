3a66707599a650f0d88d2b393f284c72ea4c9d75
InspecteurGadget <maltaesousa@gmail.com>
2016-01-27 18:53:20 +0100

    Working buttons list

--- src/LearnKorean/MainWindow.java
+++ src/LearnKorean/MainWindow.java
@@ -1,15 +1,23 @@
 package LearnKorean;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame implements ActionListener {
 
+	private LearnPanel learnPanel;
+	
+	
 public MainWindow(String title) {
     super(title);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     
+    learnPanel = new LearnPanel(this);
+    add(learnPanel, BorderLayout.CENTER);
+    
+    pack();
     setVisible(true);
 
   }
