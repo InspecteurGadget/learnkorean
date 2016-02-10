8fe2dd693f75d3158e89bb141dacf7cdcce685c8
InspecteurGadget <maltaesousa@gmail.com>
2016-02-10 23:26:38 +0100

    Fin de l'applcation

--- src/LearnKorean/LBtn.java
+++ src/LearnKorean/LBtn.java
@@ -22,15 +22,40 @@ public class LBtn extends JButton{
 		type = t;
 		setFont(new Font("Arial Unicode MS", Font.PLAIN, 48));
 	}
+	public LBtn (LBtn bouton){
+		super();
+		letter = (bouton.letter);
+		name = (bouton.name);
+		pronun = (bouton.pronun);
+		type = (bouton.type);
+		setText(letter);
+		setFont(new Font("Arial Unicode MS", Font.PLAIN, 48));
+	}
 	public void enableToolTip() {
 		setToolTipText(pronun + " (" + name + ")");
+	}
+	public void setTransparent() {
+		setContentAreaFilled(false);
+		setBorderPainted(false);
+	}
+	public void setWrong() {
+		setBackground(Color.RED);
+		setOpaque(true);
+	}
+	public void setTextAsPronun() {
+		setText(pronun);
 	}
 	public static void doPlay(LBtn bouton){
 		String audioFile = "audio/" + bouton.letter + ".mp3";
 		AudioClip mediaPlayer = new AudioClip(Paths.get(audioFile).toUri().toString());
 		mediaPlayer.play();
 	}
-	public static void getType(LBtn bouton){
+	public static String getType(LBtn bouton){
+		return bouton.type;
 		
+	}
+	public boolean equals(LBtn bouton){
+		return (bouton.name == name);
+
 	}
 }
--- src/LearnKorean/QuizzWindow.java
+++ src/LearnKorean/QuizzWindow.java
@@ -1,5 +1,6 @@
 package LearnKorean;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,39 +8,40 @@ import java.util.Arrays;
 
 import javax.swing.*;
 
-public class QuizzWindow extends JFrame implements ActionListener {
+public class QuizzWindow extends JFrame{
 	
 	private Question quizzPanel;
 	private LBtn[] list;
-	private int count;
+	private int count, nQuest = 10, right = 0, wrong = 0;
 	
 	public QuizzWindow() {
 		super("Quizz");
+		setPreferredSize(new Dimension(500, 600));
 		setLocationRelativeTo(null);
 		setVisible(true);		
 		
 		list = new LetterList().getLetterList();
 		LetterList.shuffleArray(list);
-		list = Arrays.copyOfRange(list, 0, 10);
+		list = Arrays.copyOfRange(list, 0, nQuest);
 		count = 0;
-		nextQuestion(count);
-		/*for (LBtn i : list) {
-			nextQuestion(i);
-			i.addActionListener(this);
-		}*/
-		
+		nextQuestion();		
 	}
-	public void nextQuestion(int count) {
-		if (count < 10){
+	public void nextQuestion() {
+		count++;
+		if (count < nQuest){
 			quizzPanel = new Question(this, list[count]);
-			count++;
 		}
-		//getContentPane().repaint();
+		else {
+			System.out.println(right+" "+wrong+" "+ count);
+			quizzPanel = new Question(this, right, wrong, nQuest);
+		}
+		getContentPane().repaint();
 		pack();
 	}
-	
-	public void actionPerformed(ActionEvent e) {
-		Object src = e.getSource();
+	public void addWrong() {
+		wrong++;
 	}
-
-}
+	public void addRight() {
+		right++;
+	}
+	}
